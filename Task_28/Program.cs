9bf9efaacd35517bb6fdba9a46990b8d91fa33b6
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-25 01:19:42 +0300

    Task 28 added

---
+++ Task_28/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вхoд число N и выдает произведение чисел от 1 до N */
+
+/* Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+
+for (int i = 1; i <= number; i++) {
+    product = product * i;
+}
+Console.Write($"The factorial of number is {product}");*/
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+int i = 1;
+
+while (i <= number) {
+    product = product * i;
+    i ++;
+}
+Console.Write($"The factorial of number is {product}");
