3f16864dc2d725387f54697f2f0992709b558d48
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-25 00:56:22 +0300

    Task 26 added

---
+++ Task_26/Program.cs
@@ -0,0 +1,12 @@
+﻿/* напишите программу, которая принимает на вход число и выдает количество цифр в числе*/
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (number != 0) {
+    number = number / 10;
+    count ++;
+}
+Console.Write($"Total amount of digits in number is {count}");
+
