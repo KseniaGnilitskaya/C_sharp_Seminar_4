fd93b420b19fef7e20ff2cefe96af9309d66dea3
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-25 00:58:17 +0300

    Task 26 updated

--- Task_26/Program.cs
+++ Task_26/Program.cs
@@ -4,9 +4,10 @@ Console.Write("Enter the number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 
-while (number != 0) {
-    number = number / 10;
+while (number != 0) {  // пока число не равно 0, тогда можем делить его на 10
+    number = number / 10; // деление на 10  это первая цифра
     count ++;
 }
+
 Console.Write($"Total amount of digits in number is {count}");
 
