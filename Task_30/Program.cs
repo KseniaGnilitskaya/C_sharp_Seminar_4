b1f05d082d15d303e09a0b508960057a7d073879
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-25 22:34:47 +0300

    Task 30 added

---
+++ Task_30/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] array = new int[8];
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(0, 2);
+    Console.Write($"{array[i]} ");
+}
+Console.Write("]");
