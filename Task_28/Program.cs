7586b30de83036d9fc3ebc4ec05f3557d5519a2b
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-25 22:34:30 +0300

    Tadk 28 updated

--- Task_28/Program.cs
+++ Task_28/Program.cs
@@ -7,7 +7,9 @@ int product = 1;
 for (int i = 1; i <= number; i++) {
     product = product * i;
 }
-Console.Write($"The factorial of number is {product}");*/
+Console.Write($"The factorial of number is {product}");
+
+
 
 Console.Write("Enter the number: ");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -18,4 +20,18 @@ while (i <= number) {
     product = product * i;
     i ++;
 }
-Console.Write($"The factorial of number is {product}");
+Console.Write($"The factorial of number is {product}");*/
+
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int fact(number) {
+    int result = 1;
+
+    for (int i = 1; i <= number; i++){
+        result *= 1;
+    }
+    return result;
+}
+Console.Write($"The factorial of number is {fact{number}}");
